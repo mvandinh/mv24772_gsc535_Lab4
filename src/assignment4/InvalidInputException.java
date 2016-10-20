847b9764216cd668a6e74648d15c3f26a32bdaab
mvandinh <minh.vandinh@utexas.edu>
2016-10-19 22:42:28 -0500

    How 2 Add Files 101

---
+++ src/assignment4/InvalidInputException.java
@@ -0,0 +1,27 @@
+/* CRITTERS InvalidInputException.java
+ * EE422C Project 4 submission by
+ * Minh Van-Dinh
+ * mv24772
+ * 16475
+ * Garrett Custer
+ * gsc535
+ * 16475
+ * Slip days used: <0>
+ * Git URL: https://github.com/mvandinh/mv24772_gsc535_Lab4
+ * Fall 2016
+ */
+
+package assignment4;
+
+public class InvalidInputException extends Exception {
+	String input;
+	
+	public InvalidInputException(String input) {
+		this.input = input;
+	}
+	
+	public String toString() {
+		return "Invalid Input: " + input;
+	}
+
+}
